2cca7b042cc713e674076dfdaf9f7404833d1c80
Anthony Rosati <anthony_rosati@yahoo.com>
2025-06-25 21:31:50 -0500

    initial adding and removing of items

--- src/components/RandomChoice.jsx
+++ src/components/RandomChoice.jsx
@@ -1,3 +1,53 @@
+import { useEffect, useState } from "react";
+import { InputText } from "primereact/inputtext";
+import { Button } from "primereact/button";
+
 export default function RandomChoice() {
-  return <>Potato</>;
+  const [choiceCount, setChoiceCount] = useState(1);
+  const [choices, setChoices] = useState([" "]);
+
+  const handleChoiceCountChange = ({ add = true }) => {
+    if (add) return setChoiceCount((prev) => prev + 1);
+
+    if (choiceCount <= 1) return setChoiceCount(1);
+
+    setChoiceCount((prev) => prev - 1);
+    const choiceArray = [...choices];
+    choiceArray.pop();
+    setChoices(choiceArray);
+  };
+
+  return (
+    <>
+      <div>
+        Choices:{" "}
+        {choices.map((choice, index) => (
+          <div key={index}>{choice}</div>
+        ))}
+      </div>
+
+      <Button
+        icon="pi pi-plus"
+        onClick={() => handleChoiceCountChange({ add: true })}
+      />
+      <Button
+        icon="pi pi-minus"
+        onClick={() => handleChoiceCountChange({ add: false })}
+      />
+
+      {Array.from({ length: choiceCount }).map((_, index) => {
+        return (
+          <InputText
+            key={index}
+            onChange={(e) => {
+              const newChoice = e.target.value;
+              const choiceArray = [...choices];
+              choiceArray[index] = newChoice;
+              setChoices(choiceArray);
+            }}
+          />
+        );
+      })}
+    </>
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { PrimeReactProvider } from "primereact/api";
 import RandomChoice from "./components/RandomChoice.jsx";
+
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -18,6 +23,8 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <PrimeReactProvider>
+      <RouterProvider router={router} />
+    </PrimeReactProvider>
   </StrictMode>
 );
