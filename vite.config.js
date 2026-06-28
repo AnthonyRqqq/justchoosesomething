c47cc239e0e0a6cd86e9e116603f433a5c0f7d8c
AnthonyRqqq <138615820+AnthonyRqqq@users.noreply.github.com>
2026-06-27 20:04:09 -0500

    PWA Integration (#3)

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,61 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+
+    VitePWA({
+      registerType: "autoUpdate",
+
+      manifest: {
+        name: "Just Choose Something",
+        short_name: "Choose",
+        description: "An app to help randomly choose from a list of options.",
+        start_url: "/",
+        scope: "/",
+        display: "standalone",
+        background_color: "#000000",
+        theme_color: "rgba(214, 150, 150, 0.3)",
+
+        icons: [
+          {
+            src: "/icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/icons/icon-192x192-maskable.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "/icons/icon-512x512-maskable.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+        navigateFallback: "/index.html",
+      },
+    }),
+  ],
+
   server: {
     port: 3000,
-    historyApiFallback: true,
   },
 });
